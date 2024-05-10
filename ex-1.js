355aecf316ee81c82107969c66ff5552b66afee4
Jakkaphob <jkpbeam@gmail.com>
2024-05-10 12:23:19 +0700

    feat: add javascript object assignmennt 1-3 as feature/beam

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,16 @@
 // Exercise #1: Food Order
 
 // Start coding here
+
+const foodOrder = {
+  orderNumber: "A0234",
+  address: "Bangkok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restaurantName: "Mk",
+};
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log(foodOrder);
